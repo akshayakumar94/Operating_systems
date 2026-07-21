51eb510d598bc37f489c4a7dbb786bcd3d061013
akshayakumar94 <lpperumalraj@gmail.com>
2026-07-21 16:17:46 +0530

    Msg passing receiver program

---
+++ receiver.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+struct msg_buffer
+{
+    long msg_type;
+    char msg_text[100];
+};
+
+int main()
+{
+    key_t key;
+    int msgid;
+
+    struct msg_buffer message;
+
+    key = ftok("msgqueue", 65);
+
+    msgid = msgget(key, 0666 | IPC_CREAT);
+
+    msgrcv(msgid,
+           &message,
+           sizeof(message.msg_text),
+           1,
+           0);
+
+    printf("Message Received : %s", message.msg_text);
+
+    msgctl(msgid, IPC_RMID, NULL);
+
+    return 0;
+}
